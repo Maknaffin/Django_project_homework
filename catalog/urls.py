2991e98a81154022a0f18fdb7683b16baf93a878
timofey khlystov <tima.khlystov99@mail.ru>
2023-12-10 03:31:43 +0300

    Initial commit

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,8 +1,15 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.urls import path
 
-from catalog.views import index, contact
+from catalog.apps import CatalogConfig
+from catalog.views import index, catalog_auto, auto_info
+
+app_name = CatalogConfig.name
 
 urlpatterns = [
-    path('', index),
-    path('/contacts/', contact)
-]
+    path('', index, name='index'),
+    path('<int:pk>/catalog_auto/', catalog_auto, name='catalog_auto'),
+    path('<int:pk>/auto_info/', auto_info, name='auto_info'),
+    # path('/contacts/', contact)
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- catalog/views.py
+++ catalog/views.py
@@ -1,14 +1,38 @@
 from django.shortcuts import render
 
+from catalog.models import Category, Product
+
 
 def index(request):
-    return render(request, 'catalog/home.html')
+    context = {
+        'object_list': Category.objects.all(),
+        'title': 'ВЫБЕРИТЕ ДЛЯ СЕБЯ',
+    }
+    return render(request, 'catalog/index.html', context)
 
 
-def contact(request):
-    if request.method == 'POST':
-        name = request.POST.get('name')
-        phone = request.POST.get('phone')
-        message = request.POST.get('message')
-        print(f'You have new message from {name}({phone}): {message}')
-    return render(request, 'catalog/contacts.html')
+def catalog_auto(request, pk):
+    category_item = Category.objects.get(pk=pk)
+    context = {
+        'object_list': Product.objects.filter(category_id=pk),
+        'title': f'Автомобили {category_item.name}а',
+    }
+    return render(request, 'catalog/catalog_auto.html', context)
+
+
+def auto_info(request, pk):
+    category_item = Product.objects.get(pk=pk)
+    context = {
+        'object': category_item,
+        'title': f'Подробнее о {category_item.name}'
+    }
+    return render(request, 'catalog/auto_info.html', context)
+
+
+# def contact(request):
+#     if request.method == 'POST':
+#         name = request.POST.get('name')
+#         phone = request.POST.get('phone')
+#         message = request.POST.get('message')
+#         print(f'You have new message from {name}({phone}): {message}')
+#     return render(request,
